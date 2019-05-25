a36fe1f6ada3a3ddc25b1a6a3f31a6b35498cda2
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-25 14:28:57 +0530

    Initial Commit

--- PlistSample/ViewController.swift
+++ PlistSample/ViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
-        if let path = Bundle.main.path(forResource: "Player", ofType: "plist")
-        {
-            if let array = NSDictionary(contentsOfFile: path) as? [String : Any]
-            {
-                print(array)
-            }
-    super.viewDidLoad()
+        let path  =  Bundle.main.path(forResource: "Player", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!) as! [String: Any]
+        if let arr = (dict["Contacts"] as? NSArray) {
+            let arr1 = arr
+            for  ar in arr1 {
+                let d1 = ar as! [String: Any]
+                print(d1["id"] ?? "Can't Find")
+                print(d1["name"] ?? "Can't Find")
+                let d2 = d1["phone"] as? [String: Any]
+                print(d2?["mobile"] ?? "Can't Find")
+    }
     }
     }
     override func didReceiveMemoryWarning() {
@@ -24,4 +29,3 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
-
