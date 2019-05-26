d97bc36ed27842f8ec71ab68a195ac0d2c1c9114
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-26 13:29:54 +0530

    Initial Commit

---
+++ PlistSample/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  PlistSample
+//
+//  Created by Greeens5 on 26/05/19.
+//  Copyright © 2019 Book. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PlistSample/ViewController.swift
+++ PlistSample/ViewController.swift
@@ -7,22 +7,30 @@
 //
 
 import UIKit
-class ViewController: UIViewController {
-    
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newid.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.label.text = newid[indexPath.row]
+        return cell
+    }
+    var newid : [String] = []
     override func viewDidLoad() {
         let path  =  Bundle.main.path(forResource: "Player", ofType: "plist")
         let dict = NSDictionary(contentsOfFile: path!) as! [String: Any]
-        if let arr = (dict["Contacts"] as? NSArray) {
-            let arr1 = arr
-            for  ar in arr1 {
+        if let arr = (dict["Contacts"] as? [Any]) {
+            for  ar in arr {
                 let d1 = ar as! [String: Any]
+                newid.append(d1["name"] as! String)
                 print(d1["id"] ?? "Can't Find")
                 print(d1["name"] ?? "Can't Find")
                 let d2 = d1["phone"] as? [String: Any]
                 print(d2?["mobile"] ?? "Can't Find")
-    }
-    }
+                print("newid-->",newid)
+                        }
+            }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
